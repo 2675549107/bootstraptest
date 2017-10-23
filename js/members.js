4e975cc4bba0e6d56570bdd30b57e04180b169e3
WangJinlong <wangjinlongwork@163.com>
2017-10-23 22:25:02 +0800

    更新我的账户界面原型

--- js/members.js
+++ js/members.js
@@ -21,41 +21,6 @@ $(window).resize(function () {
     $("#div_article-bg-left-under").css("height", height - 325 + "px");
 });
 
-// 局部刷新
-//$(function() {
-//    $(".account-list li").click(function() {
-//        var sId = $(this).data("id");
-//        window.location.hash = sId;
-//        var pathn;
-//        var sId = $(this).index();
-//        switch (sId) {
-//            case 0:
-//                pathn = "user_center.html";
-//                break;
-//            case 1:
-//                pathn = "user_investment.html";
-//                break;
-//            case 2:
-//                pathn = "user_fund.html";
-//                break;
-//            case 3:
-//                pathn = "user_bill.html";
-//                break;
-//            case 4:
-//                pathn = "user_information.html";
-//                break;
-//            case 5:
-//                pathn = "user_setting.html";
-//                break;
-//            default :
-//                pathn = "user_center.html";
-//                break;
-//        }
-//        $("#div_article-bg-right").load(pathn);
-//
-//    });
-//})
-
 $(function () {
     $(".account-list").on("click", "li", function () {
         var sId = $(this).data("id");
@@ -68,7 +33,6 @@ $(function () {
         switch (sId) {
             case "#center":
                 pathn = "user_center.html";
-
                 i = 0;
                 break;
             case "#investment":
@@ -83,13 +47,13 @@ $(function () {
                 pathn = "user_bill.html";
                 i = 3;
                 break;
-            case "#information":
-                pathn = "user_information.html";
-                i = 4;
-                break;
+            //case "#information":
+            //    pathn = "user_information.html";
+            //    i = 4;
+            //    break;
             case "#setting":
                 pathn = "user_setting.html";
-                i = 5;
+                i = 4;
                 break;
             default :
                 pathn = "user_center.html";
