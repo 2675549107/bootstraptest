176f889fee5e9745f41e444bd04c37d2a539e976
WangJinlong <wangjinlongwork@163.com>
2017-10-24 12:39:51 +0800

    删除无效代码,修改title,添加title图标.

--- js/members.js
+++ js/members.js
@@ -2,7 +2,6 @@
  *
  * Created by wjl on 2017/10/18.
  */
-
 // 页面载入时,布局自适应窗口高度
 $(function () {
     var width = $(window).width();
@@ -11,12 +10,10 @@ $(function () {
     $("#div_article-bg-right").css("height", height - 125 + "px");
     $("#div_article-bg-left-under").css("height", height - 325 + "px");
     $("footer").css({
-        "top" : height + "px",
-        "width" : width + "px",
-        "z-index" : 1
+        "top" : height + 300 + "px",
+        "width" : width + "px"
     });
 });
-
 // 窗口缩放时,布局自适应窗口高度
 $(window).resize(function () {
     var width = $(window).width();
@@ -25,12 +22,11 @@ $(window).resize(function () {
     $("#div_article-bg-right").css("height", height - 125 + "px");
     $("#div_article-bg-left-under").css("height", height - 325 + "px");
     $(".img_footer").css({
-        "top" : height + "px",
-        "left" : 0,
+        "top" : height + 300 + "px",
         "width" : width + "px"
     });
 });
-
+// 页面局部刷新
 $(function () {
     $(".account-list").on("click", "li", function () {
         var sId = $(this).data("id");
